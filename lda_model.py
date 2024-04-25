ea32315c79c727a09fd2b08c8e3cfc0a5f3fbcb7
udemir01 <136197213+udemir01@users.noreply.github.com>
2024-04-25 20:28:14 +0300

    added better testing, need to test with different scalers

--- lda_model.py
+++ lda_model.py
@@ -2,16 +2,27 @@ import sys
 import string
 import re
 import numpy as np
+import pandas as pd
 import numba as nb
+
 from collections import Counter
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
+from sklearn.model_selection import train_test_split, GridSearchCV
+from imblearn.metrics import classification_report_imbalanced
+from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import StandardScaler
+from imblearn.pipeline import Pipeline
+from imblearn.under_sampling import NearMiss
+from sklearn.svm import SVC
+
 translator = str.maketrans("", "", string.punctuation)
 stop_words = stopwords.words("english")
 stop_words = stop_words + ["subject", "com", "are", "edu", "would", "could"]
 lemmatizer = WordNetLemmatizer()
+scaler = StandardScaler()
 
 def file_read(filename):
     with open(filename, "r") as file:
@@ -34,11 +45,11 @@ def pre_process(data):
 
     return data, word_freq
 
-def generate_vocab(word_freq):
+def generate_vocab(word_freq, threshold=0):
     vocabulary, vocabulary_id2word = {}, {}
 
     for j, word in enumerate(word_freq):
-        if word_freq[word] >= 3:
+        if word_freq[word] >= threshold:
             vocabulary[word] = j
             vocabulary_id2word[j] = word
 
@@ -78,6 +89,7 @@ def gibbs_sampling(corpus, num_iter=200):
 
     doc_topic_count = np.sum(topic_dist_over_doc, axis=1)
     topic_word_count = np.sum(word_dist_over_topic, axis=1)
+    #doc_topic_count = np.zeros((NUMBER_OF_DOCUMENTS))
 
     for _ in nb.prange(num_iter):
         for d, doc in enumerate(corpus):
@@ -92,6 +104,7 @@ def gibbs_sampling(corpus, num_iter=200):
                 p_t_w = (word_dist_over_topic[:, w] + BETA) / (topic_word_count + VOCAB_SIZE * BETA)
                 p_z = p_d_t * p_t_w
                 p_z /= np.sum(p_z)
+                #p_z = np.abs(topic_dist_over_doc[d, :] + ALPHA) * (word_dist_over_topic[:, word] + BETA) / (topic_word_count[:] + BETA * VOCAB_SIZE)
                 new_topic = np.random.multinomial(1, p_z).argmax()
 
                 topic_assignment[d][w] = new_topic
@@ -102,6 +115,7 @@ def gibbs_sampling(corpus, num_iter=200):
 
     return topic_assignment, topic_dist_over_doc, word_dist_over_topic, doc_topic_count, topic_word_count
 
+np.random.seed(42)
 data = file_read(sys.argv[1])
 data, word_freq = pre_process(data)
 vocabulary, vocabulary_id2word = generate_vocab(word_freq)
@@ -113,7 +127,41 @@ NUMBER_OF_TOPICS = 15
 ALPHA = 0.1
 BETA = 0.1
 
-z_assignment, ndk, nkw, nd, nk = gibbs_sampling(corpus, num_iter=200)
+z_assignment, ndk, nkw, nd, nk = gibbs_sampling(corpus, num_iter=20000)
+
+metadata = pd.read_csv("dataset/metadata.txt", header=None, sep="\\s+")
+metadata.columns = ["date", "rid", "uid", "pid", "label", "a", "b", "c", "rate"]
 
 feature_vectors = ndk / nd.reshape(NUMBER_OF_DOCUMENTS, 1)
+feature_vectors = scaler.fit_transform(feature_vectors)
+labels = np.array(metadata["label"].values)
+labels = labels == "N"
 
+x_train, x_test, y_train, y_test = train_test_split(feature_vectors, labels, test_size=0.3, random_state=42)
+
+pipe = Pipeline([
+    ("sampler", NearMiss(sampling_strategy=0.66666666666)),
+    ("clf", SVC(random_state=42))
+])
+
+grid = GridSearchCV(
+    pipe,
+    param_grid = {
+        'clf__C': [0.1, 1, 10, 100, 1000],
+        'clf__gamma': [1, 0.1, 0.01, 0.001, 0.0001],
+        'clf__kernel': ['rbf']
+    },
+    scoring = "f1",
+    n_jobs = -1,
+    verbose = 3
+)
+
+grid.fit(x_train, y_train)
+
+print(grid.best_params_)
+print(grid.best_score_)
+
+y_pred = grid.predict(x_test)
+
+print(classification_report_imbalanced(y_test, y_pred))
+print("ROC-AUC: ", roc_auc_score(y_test, y_pred))
